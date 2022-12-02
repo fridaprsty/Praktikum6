428bced5c2337c91f862a5641fb5839fdf34b724
farida prsetya <faridaprsty16@gmail.com>
2022-12-02 23:12:39 +0700

    menambahkan file bangundatar.java

---
+++ bangundatar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bangundatar;
+
+public abstract class bangundatar {
+String warna;
+public String getWarna() {
+return this.warna;
+}
+public void setWarna(String warna) {
+this.warna = warna;
+}
+public abstract void gambar();
+public abstract float luas();
+}
